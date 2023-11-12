a8672567ce7bb1474e322d0d14673ad26875f9b6
beomi0607 <tkdqjay384@naver.com>
2023-11-12 18:07:12 +0900

    newvesion

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-import firstjpg from './1.png'
-import secondjpg from './2.png'
-import thirdjpg from './3.png'
-import fourthjpg from './4.png'
-import fifthjpg from './5.png'
-import sixthjpg from './6.png'
-import seventhjpg from './7.png'
-import eightthjpg from './8.png'
-import ninethjpg from './9.png'
-import tenthjpg from './10.png'
-import eleventhjpg from './11.png'
 import React, { useState, useEffect } from 'react';
 import { Router, useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4'
+import one from './images/1.png';
+import two from './images/2.png';
+import three from './images/3.png';
+import four from './images/4.png';
+import five from './images/5.png';
+import six from './images/6.png';
+import seven from './images/7.png';
+import eight from './images/8.png';
+import nine  from './images/9.png';
+import ten from './images/10.png';
+import eleven from './images/11.png';
+import twelve from './images/12.png';
+import thirteen from './images/13.png';
+import fourteen from './images/14.png';
+import fiftheen from './images/15.png';
+import sixteen from './images/16.png';
+import seventeen from './images/17.png';
+import eighteen from './images/18.png';
+import nintheen from './images/19.png';
+import twenty from './images/20.png';
+import twentyone from './images/21.png';
+import twentytwo from './images/22.png';
+import twentythree from './images/23.png';
+import twentyfour from './images/24.png';
+
+import twentysix from './images/26.png';
+import twentyseven from './images/27.png';
+import twentyeight from './images/28.png';
+import twentynine from  './images/29.png';
 
 
 ReactGA.initialize("G-0RCDWEEBWK")
@@ -85,11 +103,11 @@ function App() {
     ReactGA.event({
       category: 'Button',
       action: 'Click',
-      label: '더 알아보기',
+      label: '선생님 더 알아보기',
     });
 
     // Redirect to the specified URL
-    window.location.href = 'https://m.place.naver.com/place/1216958182/ticket?entry=pll';
+    window.location.href = 'https://m.booking.naver.com/booking/10/bizes/996848/items/5452772?area=pll';
   };
 
   const handleButtonClick2 = () => {
@@ -101,7 +119,7 @@ function App() {
     });
 
     // Redirect to the specified URL
-    window.location.href = 'https://m.place.naver.com/place/1216958182/ticket?entry=pll';
+    window.location.href = 'https://m.booking.naver.com/booking/10/bizes/996848/items/5452772?area=pll';
   };
 
 
@@ -115,46 +133,57 @@ function App() {
     });
 
     // Redirect to the specified URL
-    window.location.href = 'https://m.place.naver.com/place/1216958182/ticket?entry=pll';
+    window.location.href = 'https://m.booking.naver.com/booking/10/bizes/996848/items/5452772?area=pll';
   };
  
+
 
   return (
 
     <div className="App">
-        <img src={firstjpg} style={{width:'100%',height:'100%'}}/>
-        <img src={secondjpg} style={{width:'100%',height:'100%'}}/>
-        <img src={thirdjpg} style={{width:'100%',height:'100%'}}/>
-        <img src={fourthjpg} style={{width:'100%',height:'100%'}}/>
-        <img src={fifthjpg} style={{width:'100%',height:'100%'}}/>
-        <img src={sixthjpg} style={{width:'100%',height:'100%'}}/>
-        <img src={seventhjpg} style={{width:'100%',height:'100%'}}/>
-        <img src={eightthjpg} style={{width:'100%',height:'100%'}}/>
-        <div style={{ width: '100%', height: 100, backgroundColor: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <button style={{borderColor:'blue',borderRadius:30,width:'30%',height:'40%', fontWeight:'bold',color: 'blue',backgroundColor:'white', animation: 'shake 0.5s' }} onClick={handleButtonClick1}>
-              더 알아보기
-          </button>
-        </div>
-        <div style={{ width: '100%', height: 100, backgroundColor: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        </div>     
-        <img src={ninethjpg} style={{width:'100%',height:'100%'}}/>
-        <div style={{ width: '100%', height: 100, backgroundColor: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <button style={{borderColor:'blue',borderRadius:30,width:'30%',height:'40%',fontWeight:'bold',color: 'blue',backgroundColor:'white', animation: 'shake 0.5s' }} onClick={handleButtonClick2}>
-            PT 신청하기
-          </button>
-        </div>
-        <div style={{ width: '100%', height: 100, backgroundColor: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        </div>  
-        <img src={tenthjpg} style={{width:'100%',height:'100%'}}/>
-        <div style={{ width: '100%', height: 100, backgroundColor: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        </div>     
-        <img src={eleventhjpg} style={{width:'100%',height:'100%'}}/>
-        <style>{shakeKeyframes}</style>
-        <div style={{ width: '100%', height: 100, backgroundColor: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <button style={{borderColor:'white',borderRadius:30,width:'30%',height:'40%',color: 'white',backgroundColor:'blue', animation: 'shake 0.5s' }} onClick={handleButtonClic3}>
+      <img src={one}  style={{width:'100%',height:'100%'}} />
+      <div style={{ width: '100%', height: 100, backgroundColor: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <button style={{borderColor:'blue',borderRadius:30,width:'30%',height:'40%',fontWeight:'bold',color: 'blue',backgroundColor:'white', animation: 'shake 0.5s' }} onClick={handleButtonClick1}>
+              선생님 더 알아보기
+        </button>
+      </div>
+      <img src={two}  style={{width:'100%',height:'100%'}} />
+      <img src={three}  style={{width:'100%',height:'100%'}} />
+      <img src={four}  style={{width:'100%',height:'100%'}} />
+      <img src={five}  style={{width:'100%',height:'100%'}} />
+      <img src={six}  style={{width:'100%',height:'100%'}} />
+      <img src={seven}  style={{width:'100%',height:'100%'}} />
+      <img src={eight}  style={{width:'100%',height:'100%'}} />
+      <img src={nine}  style={{width:'100%',height:'100%'}} />
+      <img src={ten}  style={{width:'100%',height:'100%'}} />
+      <img src={eleven}  style={{width:'100%',height:'100%'}} />
+      <img src={twelve}  style={{width:'100%',height:'100%'}} />
+      <img src={thirteen}  style={{width:'100%',height:'100%'}} />
+      <img src={fourteen}  style={{width:'100%',height:'100%'}} />
+      <img src={fiftheen}  style={{width:'100%',height:'100%'}} />
+      <img src={sixteen}  style={{width:'100%',height:'100%'}} />
+      <img src={seventeen}  style={{width:'100%',height:'100%'}} />
+      <img src={eighteen}  style={{width:'100%',height:'100%'}} />
+      <img src={nintheen}  style={{width:'100%',height:'100%'}} />
+      <img src={twenty}  style={{width:'100%',height:'100%'}} />
+      <img src={twentyone}  style={{width:'100%',height:'100%'}} />
+      <img src={twentytwo}  style={{width:'100%',height:'100%'}} />
+      <img src={twentythree}  style={{width:'100%',height:'100%'}} />
+      <img src={twentyfour}  style={{width:'100%',height:'100%'}} />
+      <div style={{ width: '100%', height: 100, backgroundColor: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <button style={{borderColor:'blue',borderRadius:30,width:'30%',height:'40%',fontWeight:'bold',color: 'blue',backgroundColor:'white', animation: 'shake 0.5s' }} onClick={handleButtonClick2}>
+              PT 신청하기
+        </button>
+      </div>
+      <img src={twentysix}  style={{width:'100%',height:'100%'}} />
+      <img src={twentyseven}  style={{width:'100%',height:'100%'}} />
+      <div style={{ width: '100%', height: 50, backgroundColor: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <button style={{borderColor:'blue',borderRadius:30,width:'30%',height:'80%',fontWeight:'bold',color: 'blue',backgroundColor:'white', animation: 'shake 0.5s' }} onClick={handleButtonClick2}>
               지금 무료 체험하기
-          </button>
-        </div>   
+        </button>
+      </div>
+      <img src={twentyeight}  style={{width:'100%',height:'100%'}} />
+      <img src={twentynine}  style={{width:'100%',height:'100%'}} />
     </div>
   );
 }
